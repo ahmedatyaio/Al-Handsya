1217569b16ffdb1c7c6add17d3e2dd72d4307796
Ahmed Atiyah <ahmedmaheratiyah@gmail.com>
2019-09-16 06:22:05 +0200

    setting up project plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,42 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
     title: `Al-Handsya for Construction and Real Estate`,
     description: `Al-Handsya is a leading construction and real estate company based in Cairo, EG`,
-    author: `@alhandsya`,
+    author: `@alhandsya`
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: ['source sans pro:600', 'montserrat:700,900'],
+        display: 'swap'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        color: '#fdd835'
+      }
+    },
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+        path: path.join(__dirname, `src`, `images`)
+      }
+    }
+  ]
+};
