45d668be9ab39dfc512eb2a68ed722f472b7bf8d
Ahmed Atiyah <ahmedmaheratiyah@gmail.com>
2019-09-16 05:31:31 +0200

    initial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Al-Handsya for Construction and Real Estate`,
+    description: `Al-Handsya is a leading construction and real estate company based in Cairo, EG`,
+    author: `@alhandsya`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
